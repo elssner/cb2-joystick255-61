8acc87dbdb8e1c753a4610e83bad11bc651da157
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-12-10 19:45:33 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonEvent(Button.A, input.buttonEventValue(ButtonEvent.LongClick), function () {
+    radio.setGroup(223)
+    basic.showString("A")
+})
+input.onButtonEvent(Button.AB, input.buttonEventValue(ButtonEvent.LongClick), function () {
+    radio.setGroup(222)
+    basic.showString("C")
+})
+input.onButtonEvent(Button.B, input.buttonEventValue(ButtonEvent.LongClick), function () {
+    radio.setGroup(224)
+    basic.showString("B")
+})
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     if (b2 > 1) {
         b2 += -1
@@ -19,6 +31,7 @@ radio.setFrequencyBand(1)
 radio.setGroup(222)
 qwiicjoystick.beimStart(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20))
 b2 = 7
+basic.showString("C")
 loops.everyInterval(500, function () {
     radio.sendNumber(qwiicjoystick.readJoystick(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20), b2))
 })
